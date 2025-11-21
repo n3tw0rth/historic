d692482dea0d16f57832b5888dd23492304f91e1
n3tw0rth <rtheekshanafernando@gmail.com>
2025-11-21 22:27:43 +0530

    fix(tui): fix hanging commands removing async from run()

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use historic::cmd::{Cli, Cmd};
 use historic::db::Db;
 use historic::terminal::Terminal;
 use historic::tui::ui::Tui;
+use historic::utils;
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
@@ -15,15 +16,25 @@ pub async fn main() -> Result<()> {
     let db = Arc::new(Db::new().await?);
 
     let cmd = Cmd::new(&args.command, terminal.clone(), db.clone()).await;
-    let mut tui = Tui::new(terminal.clone(), db.clone());
+    let mut tui = Tui::new();
+
+    let session_id = utils::string_to_md5(&format!("{:?} ", terminal));
+    let mut rows = db.get_commands(&session_id).await?;
+
+    let mut items = Vec::new();
+    while let Some(row) = rows.next().await? {
+        let r: String = row.get(4)?;
+        items.push(r);
+    }
 
     match args.command {
         None => {
             color_eyre::install()?;
             let ratatui_term = ratatui::init();
-            let result = tui.run(ratatui_term);
+            let result = tui.run(ratatui_term, items);
             ratatui::restore();
-            result.await?;
+            result?;
+            ()
         }
         _ => {
             cmd.run().await?;
--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,42 +1,23 @@
-use std::sync::Arc;
-
 use color_eyre::Result;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
 use ratatui::widgets::{Block, List, ListDirection, ListState, Paragraph};
 use ratatui::{DefaultTerminal, prelude::*};
 
-use crate::db::Db;
-use crate::terminal::Terminal;
-use crate::utils;
-
 pub struct Tui {
-    term: Arc<Terminal>,
-    db: Arc<Db>,
     cmds: Vec<String>,
     exit: bool,
 }
 
 impl Tui {
-    pub fn new(term: Arc<Terminal>, db: Arc<Db>) -> Self {
+    pub fn new() -> Self {
         Tui {
-            term,
-            db,
-            cmds: vec!["test".to_string(), "pnpm install".to_string()],
+            cmds: Vec::new(),
             exit: false,
         }
     }
 
-    pub async fn run(&mut self, mut term: DefaultTerminal) -> Result<()> {
-        let session_id = utils::string_to_md5(&format!("{:?} ", self.term));
-        let mut rows = self.db.get_commands(&session_id).await?;
-
-        let mut items = Vec::new();
-        while let Some(row) = rows.next().await? {
-            let r: String = row.get(4)?;
-            items.push(r);
-        }
-
-        self.cmds = items;
+    pub fn run(&mut self, mut term: DefaultTerminal, cmds: Vec<String>) -> Result<()> {
+        self.cmds = cmds;
         while !self.exit {
             term.draw(|frame| self.render(frame))?;
             self.handle_events()?;
@@ -45,12 +26,9 @@ impl Tui {
     }
 
     fn handle_events(&mut self) -> Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
+        if let Event::Key(key) = event::read()? {
+            self.handle_key_event(key);
+        }
         Ok(())
     }
 
